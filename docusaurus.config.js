734eaaa96b7edb2a8b559e0f7c7d8ee7154ef5e4
Alvin B <al-v-in@users.noreply.github.com>
2020-11-05 14:19:00 +0000

    fix docs edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/covidgraph/docs-site/edit/master/',
         },
         blog: {
           showReadingTime: true,
