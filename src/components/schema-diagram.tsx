8c757f667098478a8bd83907bf91256b455f6f94
Jamie Munro <jamie@pop-os.localdomain>
2020-11-18 13:25:47 +0000

    Added Schema shared component and svg diagram

---
+++ src/components/schema-diagram.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react'; // importing FunctionComponent
+
+
+const SchemaDiagram: FunctionComponent = () => <aside>
+
+  <img src="../../docs/images/schema.svg"></img>  {/* Rename latest schema: schema.svg */}
+
+</aside>
+
+export default SchemaDiagram;
