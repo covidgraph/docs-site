67a9a6e0c3a48948780766e1874cbc542ac66dac
jiros <jamie@munro.consulting>
2021-03-22 21:46:16 +0000

    Component changes

---
+++ src/components/diagram-covidgraph-pipeline.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react'; // importing FunctionComponent
+import PipelineSVG from '../../docs/images/diagrams/covidgraph-pipeline.svg';
+
+const PipelineDiagram: FunctionComponent = () => <aside>
+
+<PipelineSVG />
+
+</aside>
+
+export default PipelineDiagram;
--- src/components/diagram-covidgraph-repos.tsx
+++ src/components/diagram-covidgraph-repos.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from 'react'; // importing FunctionComponent
 
 const RepoDiagram: FunctionComponent = () => <aside>
 
-  <img src="../../docs/images/covidgraph_repos.svg"></img>  {/* Rename latest overview: covidgraph_repos.svg */}
+  <img src="../../docs/images/diagrams/covidgraph-repos.svg"></img>
 
 
 </aside>
