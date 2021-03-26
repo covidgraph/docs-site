1de22249a858a286d410d2ea9a7f7246b7bb8c82
jiros <jamie@munro.consulting>
2021-03-26 11:27:08 +0000

    corrected edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,7 +96,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/covidgraph/',
+            'https://github.com/covidgraph/docs-site/tree/master/',
         },
         blog: {
           showReadingTime: true,
